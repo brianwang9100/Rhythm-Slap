758e04f62edec6fe690016f2f78b2d31870e2895
joewang704 <joewang704@gmail.com>
2014-09-21 03:17:33 -0400

    jhkh

--- RhythmSlapObjc.spritebuilder/GameEndPopUp.m
+++ RhythmSlapObjc.spritebuilder/GameEndPopUp.m
@@ -26,37 +26,37 @@
     [_gameOverColor runAction:fadeIn];
     
     _gameOverText.position = ccp(0.5, -200);
-    id gameOverDropDown = [CCActionMoveTo actionWithDuration:1 position:ccp(.5, 90)];
+    id gameOverDropDown = [CCActionMoveTo actionWithDuration:2 position:ccp(.5, 90)];
     id gameOverElasticDown = [CCActionEaseElasticInOut actionWithAction:gameOverDropDown period:.4];
     [_gameOverText runAction:gameOverElasticDown];
     
     _yourScoreTitle.position = ccp(0.5, -200);
-    id yourScoreDropDown = [CCActionMoveTo actionWithDuration:1.8 position:ccp(.5, 182.6)];
+    id yourScoreDropDown = [CCActionMoveTo actionWithDuration:3 position:ccp(.5, 182.6)];
     id yourScoreElasticDown = [CCActionEaseElasticInOut actionWithAction:yourScoreDropDown period:.4];
     [_yourScoreTitle runAction:yourScoreElasticDown];
     
-    _yourScoreNum.position = ccp(0.5, -200);
-    id scoreNumDropDown = [CCActionMoveTo actionWithDuration:1.8 position:ccp(.5, 232)];
+    _yourScoreNum.position = ccp(-1, 232);
+    id scoreNumDropDown = [CCActionMoveTo actionWithDuration:5 position:ccp(.5, 232)];
     id scoreNumElasticDown = [CCActionEaseElasticInOut actionWithAction:scoreNumDropDown period:.4];
     [_yourScoreNum runAction:scoreNumElasticDown];
     
     _highScoreTitle.position = ccp(0.5, -200);
-    id highScoreDropDown = [CCActionMoveTo actionWithDuration:2.4 position:ccp(.5, 284)];
+    id highScoreDropDown = [CCActionMoveTo actionWithDuration:3 position:ccp(.5, 284)];
     id highScoreElasticDown = [CCActionEaseElasticInOut actionWithAction:highScoreDropDown period:.4];
     [_highScoreTitle runAction:highScoreElasticDown];
     
-    _highScoreNum.position = ccp(0.5, -200);
-    id highNumDropDown = [CCActionMoveTo actionWithDuration:2.4 position:ccp(.5, 335.4)];
+    _highScoreNum.position = ccp(-1, 335.4);
+    id highNumDropDown = [CCActionMoveTo actionWithDuration:5 position:ccp(.5, 335.4)];
     id highNumElasticDown = [CCActionEaseElasticInOut actionWithAction:highNumDropDown period:.4];
     [_highScoreNum runAction:highNumElasticDown];
     
-    _tryAgain.position = ccp(0.5, 1500);
-    id tryAgainDropDown = [CCActionMoveTo actionWithDuration:3 position:ccp(.5, 130)];
+    _tryAgain.position = ccp(0.5, -200);
+    id tryAgainDropDown = [CCActionMoveTo actionWithDuration:4 position:ccp(.5, 130)];
     id tryAgainElasticDown = [CCActionEaseElasticInOut actionWithAction:tryAgainDropDown period:.4];
     [_tryAgain runAction:tryAgainElasticDown];
     
-    _mainMenu.position = ccp(0.5, 1500);
-    id mainMenuDropDown = [CCActionMoveTo actionWithDuration:3 position:ccp(.5, 70)];
+    _mainMenu.position = ccp(0.5, -200);
+    id mainMenuDropDown = [CCActionMoveTo actionWithDuration:4 position:ccp(.5, 70)];
     id mainMenuElasticDown = [CCActionEaseElasticInOut actionWithAction:mainMenuDropDown period:.4];
     [_mainMenu runAction:mainMenuElasticDown];
 }
